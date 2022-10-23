bf68e457f2064565be9330da925583e1d67f1816
kuru-shu <kurushu71@gmail.com>
2022-10-23 15:27:30 +0900

    history

---
+++ .history/src/App_20221023151756.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Home } from './Page1';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Page1 />
+      <Page2 />
+    </div>
+  );
+}
+
+export default App;
